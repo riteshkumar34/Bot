5eaeb7a3e1027ffd5a2fc9da908cc09df83c4891
riteshkumar34 <riteshandshan@gmail.com>
2025-11-22 10:30:19 +0530

    frontend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://bot-git-main-riteshkumar34s-projects.vercel.app" 
+      "https://stresschat-git-main-riteshkumar34s-projects.vercel.app" 
     ],
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
